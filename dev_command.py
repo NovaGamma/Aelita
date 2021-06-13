adce59e3c12f2cb0b66da28aa876942cf5f03c5f
NovaGamma <gammanova41@gmail.com>
2021-06-13 11:14:35 +0200

    fix spam command

--- dev_command.py
+++ dev_command.py
@@ -3,9 +3,9 @@ from init import*
 @bot.command()
 async def spam(ctx,number):
     if ctx.author.id == Elvin:
-        if type(number) is int:
-            for i in range(number):
-                await ctx.channel.send("A little spam !")
+        number = int(number)
+        for i in range(number):
+            await ctx.channel.send("A little spam !")
 
 @bot.command()
 async def getxD(ctx):
